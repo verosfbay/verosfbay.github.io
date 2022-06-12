f2e50edc490bda7dc734ca24b5cd1a27d1f7f6a4
Veronica Lobkina <you@example.com>
2022-06-12 14:03:56 -0700

    deliverable 2

--- charts.js
+++ charts.js
@@ -96,5 +96,33 @@ function buildCharts(sample) {
      };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+        // 1. Create the trace for the bubble chart.
+        var bubbleData = [{
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          }
+        }];
+    
+        // 2. Create the layout for the bubble chart.
+        var bubbleLayout = {
+          title: "Bacteria Species per Sample",
+          xaxis: {title: "OTU ID"},
+          hovermode: "closest",
+          showlegend: false
+        };
+    
+        // 3. Use Plotly to plot the data with the layout.
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
   });
 }
+
+
+
+
